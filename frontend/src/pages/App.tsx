32bc72013d6856d96099daef5ae5e49f3131057a
Sderr12 <andrea.sidero6@gmail.com>
2025-11-14 15:40:34 +0100

    refactor: refactor code in App.tsx

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <div className="h-dvh overflow-hidden flex flex-col">
